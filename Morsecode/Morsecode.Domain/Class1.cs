27841340314018413165d6f9aaa8dfc2dbfc5449
Aiden <aidenpoulton@gmail.com>
2018-01-11 15:44:24 +0000

    Stub out solution

---
+++ Morsecode/Morsecode.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Morsecode.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Morsecode/Morsecode.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Morsecode.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
