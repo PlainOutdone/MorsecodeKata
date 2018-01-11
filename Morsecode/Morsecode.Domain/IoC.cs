c2ec756522ced2904b086431c5eab11a073dbc3a
Aiden <aidenpoulton@gmail.com>
2018-01-11 16:57:52 +0000

    Add IoC & wire up integration tests
    Broken build : The integration test is actually red, issue with spaces.

---
+++ Morsecode/Morsecode.Domain/IoC.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Morsecode.Domain.CodeReader;
+using Morsecode.Domain.CodeSplitter;
+using Morsecode.Domain.Translator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morsecode.Domain
+{
+    public class IoC
+    {
+
+        private static IServiceProvider _container;
+        public static IServiceProvider Container
+        {
+            get
+            {
+                if (_container == null)
+                { _container = BuildDefaultServiceCollection().BuildServiceProvider(); }
+                return _container;
+            }
+        }
+
+        private static ServiceCollection BuildDefaultServiceCollection()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<ITranslator, DefaultTranslator>();
+            services.AddSingleton<ISplitter, DefaultSplitter>();
+            services.AddSingleton<ICodeReader, DefaultCodeReader>();
+
+            return services;
+        }
+    }
+}
+
+
+
---
+++ Morsecode/Morsecode.IntegrationTests/DefaultCodeReaderTests.cs
@@ -0,0 +1,36 @@
+﻿using Morsecode.Domain;
+using Morsecode.Domain.CodeReader;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Morsecode.IntegrationTests
+{
+    public class DefaultCodeReaderTests
+    {
+        [Fact]
+        public static void WhenGivenNoMessageReturnAnEmptyString()
+        {
+            ICodeReader reader = IoC.Container.GetService<ICodeReader>();
+            var expected = "";
+            var actual = reader.DecryptMessage("");
+
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public static void WhenGivenSomeValidMorseCodeReturnTheMessage()
+        {
+            ICodeReader reader = IoC.Container.GetService<ICodeReader>();
+            string input = ".... . -.--   .--- ..- -.. .";
+            string expected = "HEY JUDE";
+
+            var actual = reader.DecryptMessage(input);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
