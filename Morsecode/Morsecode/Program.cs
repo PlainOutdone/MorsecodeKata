de2907221f7b2e666aff3bf7a35fb7372cc9c972
Aiden <aidenpoulton@gmail.com>
2018-01-11 15:41:39 +0000

    init

---
+++ Morsecode/Morsecode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Morsecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
